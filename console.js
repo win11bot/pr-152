75ddea734b15a500c5fef43db9798e6e77c4b7c9
Posandu <Posandu@users.noreply.github.com>
2021-10-23 10:20:24 +0000

    deploy: blueedgetechno/win11React@3b4dcda43ee9ea01b964823009f55f5117578b25

---
+++ console.js
@@ -0,0 +1,5 @@
+console.log('%cWindows 11 in React is a project by blueedgetechno and ascended by andrewstech. If you wish to help out with this project, visit https://github.com/blueedgetechno/windows11', 'color: lightblue; font-size: x-large');
+
+function removeWarning() {
+  document.body.dataset.force = true;
+}
